dda40f81b89db08487d6c5a79680cbd8f782e405
kimnewbie <gracecancho@gmail.com>
2022-08-11 17:26:18 +0900

    페이지 기능 구현

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -43,10 +43,18 @@ export const write = async ctx => {
 };
 
 export const list = async ctx => {
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
         const posts = await Post.find()
             .sort({ _id: -1 }) // 내림차순 오름차순은 _id : 1
-            .limit(10)
+            .limit(10) // 10개만 보이게
+            .skip((page - 1) * 10)
             .exec();
         ctx.body = posts;
     } catch (e) {
